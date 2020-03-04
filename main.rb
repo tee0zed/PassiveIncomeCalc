0547ea6382279c8c91c7caaef7b48fcb96e11cc8
Taras <tee0zed@gmail.com>
2020-03-04 18:16:11 +0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,53 +1,41 @@
 # encoding: utf-8
 
-current_dep = 0
+def get_sum_to_buy(rub_amount, dol_amount, current_dol_rate)
+  dol_to_rub = dol_amount * current_dol_rate
 
-month_count = 0
+  difference = ((dol_to_rub - rub_amount).abs / 2).round(2)
 
-income = 0
-
-puts "Это программа подсчитает срок на который вам нужно сделать вклад чтобы получать желаемую сумму ежемесячно."
-
-puts "Какая сумма вас устроила бы ежемесячно?
-      \nВводите сумму в рублях, без копеек."
-
-desired_sum = gets.to_f
-
-while desired_sum <= 0
-  desired_sum = gets.to_f
-  puts "Вводите сумму в рублях, без копеек."
+  if difference < current_dol_rate * 0.01
+    "Портфели сбалансированы."
+  elsif rub_amount > dol_to_rub
+    "Вам нужно продать #{difference} Р."
+  else
+    "Вам нужно купить #{difference} Р."
+  end
 end
 
-puts "Какую сумму в месяц вы были бы готовы вносить на депозит?
-      \nВводите сумму в рублях, без копеек."
+def get_input
+  input = gets.to_f
 
-month_deposit = gets.to_f
-
-while month_deposit <= 0
-  month_deposit = gets.to_f
-  puts "Вводите сумму в рублях, без копеек."
+  while input == 0
+    puts "Введите только число."
+    input = gets.to_f
+  end
+  input
 end
 
-puts "Какой процент годовых по этому вкладу?"
+puts
+puts "Курс доллара?"
+current_dol_rate = get_input
 
-interest = gets.to_f
+puts
+puts "Сколько у вас рублей?"
+rub_amount = get_input
 
-while interest <= 0
-  interest = gets.to_f
-  puts "Вводите процент с точностью до десятых через точку."
-end
-# Найдем наш коэффициент в процентах:
-interest = interest * 0.01
+puts
+puts "Сколько у вас долларов?"
+dol_amount = get_input
 
-until income >= desired_sum
-  current_dep += month_deposit
-  month_count += 1
-  # Из-за того что банки считают одним месяцем 30 дней, а годом 365 -
-  # просто взять и поделить на 12 не выходит, тогда:
-  current_dep = current_dep + (current_dep * interest * 30.0) / 365.0
-  current_dep = current_dep.round(2)
-  # Посчитаем какую прибыль мы получаем в следующем месяце
-  income = (current_dep * interest * 30.0) / 365.0
-end
+result = get_sum_to_buy(rub_amount, dol_amount, current_dol_rate)
 
-puts "Вам потребуется #{month_count / 12} год(а)/лет и #{month_count % 12} месяц(а/ев)"
+puts result
